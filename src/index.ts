11f61808aef426441ce103efcabd6fa03d460f8c
Felix Breuer <f.breuer94@gmail.com>
2019-10-12 16:42:32 +0200

    backup

--- src/index.ts
+++ src/index.ts
@@ -2,36 +2,25 @@ import { IgApiClient } from 'instagram-private-api';
 import dotenv from 'dotenv';
 dotenv.config();
 
-// ------------- sample code from site ---------------------
+const { IG_USERNAME, IG_PASSWORD } = process.env;
+console.log(IG_USERNAME, IG_PASSWORD);
+
 const ig = new IgApiClient();
-// You must generate device id's before login.
-// Id's generated based on seed
-// So if you pass the same value as first argument - the same id's are generated every time
-ig.state.generateDevice(process.env.IG_USERNAME!);
-// Optionally you can setup proxy url
-ig.state.proxyUrl = process.env.IG_PROXY!;
+
+ig.state.generateDevice(IG_USERNAME!);
 
 (async () => {
-  // Execute all requests prior to authorization in the real Android application
-  // Not required but recommended
   await ig.simulate.preLoginFlow();
-  const loggedInUser = await ig.account.login(process.env.IG_USERNAME!, process.env.IG_PASSWORD!);
-  // The same as preLoginFlow()
-  // Optionally wrap it to process.nextTick so we dont need to wait ending of this bunch of requests
-  process.nextTick(async () => await ig.simulate.postLoginFlow());
-  // Create UserFeed instance to get loggedInUser's posts
-  const userFeed = ig.feed.user(loggedInUser.pk);
-  const myPostsFirstPage = await userFeed.items();
-  // All the feeds are auto-paginated, so you just need to call .items() sequentially to get next page
-  const myPostsSecondPage = await userFeed.items();
-  await ig.media.like({
-    // Like our first post from first page or first post from second page randomly
-    mediaId: sample([myPostsFirstPage[0].id, myPostsSecondPage[0].id]),
-    moduleInfo: {
-      module_name: 'profile',
-      user_id: loggedInUser.pk,
-      username: loggedInUser.username,
-    },
-    d: sample([0, 1]),
-  });
+  const loggedInUser = await ig.account.login(IG_USERNAME!, IG_PASSWORD!);
+  ig.simulate.postLoginFlow(); // dont await here
+
+  const feed2 = ig.feed.timeline();
+  const test2 = await feed2.items();
+  for (const item of test2) {
+    if (item.media_type == 1) {
+      console.log(item);
+      console.log(item.image_versions2.candidates)
+      break;
+    }
+  }
 })();
