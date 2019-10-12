6911d87e52a4f9a19e7381965deb640b64d40618
Florenz23 <florenz.erstling@lalalama.de>
2019-10-12 11:21:50 -0300

    removed files

--- ts-dist/quickstart.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const login_util_1 = require("./login_util");
-var filePath = "./data/cookies/";
-var loginUtil = new login_util_1.LoginUtil(filePath);
-loginUtil.login();
