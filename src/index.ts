50ff1eb1c92a11886c7b09c8b47e897579bc024e
felixbreuer <contact@felixbreuer.me>
2019-10-12 19:07:55 +0200

    fix linter

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,4 @@
-import { IgApiClient } from 'instagram-private-api';
-import envConfig, { IConfig } from './config';
-import session from './session';
-
-class loop {
-  private ig: IgApiClient;
-  private config: IConfig;
-  private session: any;
-
-  constructor(config?: IConfig) {
-    this.ig = new IgApiClient();
-    this.config = config || envConfig;
-    this.session = new session(this.ig, this.config);
-  }
-
-  async run() {
-    await this.session.login();
-
-    const feed2 = this.ig.feed.timeline();
-    const test2 = await feed2.items();
-    for (const item of test2) {
-      if (item.media_type == 1) {
-        console.log(item);
-        break;
-      }
-    }
-  }
-}
+import loop from './loop';
 
 const l = new loop();
 l.run();
