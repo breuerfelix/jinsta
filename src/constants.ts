6fe29b0d15719118b904f33dd6b42f4ab3cd294a
felixbreuer <contact@felixbreuer.me>
2019-10-14 10:31:36 +0200

    skip private users

--- src/constants.ts
+++ src/constants.ts
@@ -1,9 +1,11 @@
 class Constants {
 	public base_interest = .1;
-	public interest_inc = .05;
+	public interest_inc = .05; // should be adjusted if there are more / less keywords
 
 	public media_delay = 3;
 
+	// all these chances are getting multiplied by the base interest
+	// example: chance to like a picture is .1 * .5 so .05
 	public like_chance = .5;
 	//public follow_chance = .3;
 	public nested_feed_chance = .3;
