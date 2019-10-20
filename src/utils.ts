24a4f55df04ce7cfcb4e2f7bf4f38fe98d2bf180
Felix Breuer <f.breuer94@gmail.com>
2019-10-20 21:03:44 +0200

    implement store and workspaces

--- src/utils.ts
+++ src/utils.ts
@@ -1,38 +1,7 @@
-import fs from 'fs';
-
 const sleep = (seconds: number): Promise<void> => new Promise(resolve => setTimeout(resolve, seconds * 1000));
-
 const chance = (percentage: number): boolean => Math.random() < percentage;
 
-function parseSession(filepath: string): any {
-	let cookie = null;
-	let user = null;
-	let seed = null;
-	let restore = false;
-
-	if (fs.existsSync(filepath)) {
-		const content = fs.readFileSync(filepath, 'utf-8');
-		const data = JSON.parse(content);
-
-		cookie = data.cookie;
-		seed = data.seed;
-		user = data.user;
-		restore = true;
-	}
-
-	const config = {
-		cookie,
-		user,
-		seed,
-		restore,
-		sessionPath: filepath,
-	};
-
-	return config;
-}
-
 export {
-	parseSession,
 	sleep,
 	chance,
 };
