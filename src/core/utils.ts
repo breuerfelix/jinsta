6a13a480b07e181718058de2fcc73e112fc424da
felixbreuer <contact@felixbreuer.me>
2019-10-27 21:48:47 +0100

    conver media id to post id

--- src/core/utils.ts
+++ src/core/utils.ts
@@ -1,7 +1,24 @@
+import bigInt from 'big-integer';
+
 const sleep = (seconds: number): Promise<void> => new Promise(resolve => setTimeout(resolve, seconds * 1000));
 const chance = (percentage: number): boolean => Math.random() < percentage;
+
+function convertIDtoPost(mediaID: string): string {
+	let id = bigInt(mediaID.split('_', 1)[0]);
+	const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_';
+	let shortcode = '';
+
+	while (id.greater(0)) {
+		const division = id.divmod(64);
+		id = division.quotient;
+		shortcode = `${alphabet.charAt(Number(division.remainder))}${shortcode}`;
+	}
+
+	return 'https://instagram.com/p/' + shortcode;
+}
 
 export {
 	sleep,
 	chance,
+	convertIDtoPost,
 };
