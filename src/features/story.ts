647338ab1dcd5c4cd9ec88b88f13b09f5e917fe9
Luca Reccia <lucareccia@hotmail.it>
2019-11-01 20:32:44 +0100

    working

---
+++ src/features/story.ts
@@ -0,0 +1,46 @@
+import { IgApiClient } from "instagram-private-api";
+import { Config } from "../core/config";
+import { store } from "../core/store";
+import logger from "../core/logging";
+import { Feed } from "instagram-private-api/dist/core/feed";
+import { StoryServiceInput } from "instagram-private-api/dist/types";
+
+async function story(client: IgApiClient, config: Config): Promise<void> {
+	logger.info("starting with story feed");
+
+	const storyTrayFeed = client.feed.reelsTray("cold_start");
+	let storyTray = await storyTrayFeed.items();
+
+	
+	logger.info("%o",storyTray.find(item=>item.user.username==='gaetanogenovesimaestro'));
+	storyTray = storyTray.filter(
+		(item: any) => item.seen === 0 && item.user.username !== config.username
+	);
+
+	logger.info("%o",storyTray.find(item=>item.user.username==='gaetanogenovesimaestro'));
+
+	const storyMediaFeed = client.feed.reelsMedia({
+		userIds: storyTray.map(item => item.user.pk)
+	});
+
+	const stories = await storyMediaFeed.items();
+
+	const res = await client.story.seen(stories);
+
+	/*const targetUser = await client.user.searchExact("maniinpasta"); // getting exact user by login
+	const reelsFeed = client.feed.reelsMedia({
+		// working with reels media feed (stories feed)
+		userIds: [targetUser.pk] // you can specify multiple user id's, "pk" param is user id
+	});
+	const storyItems = await reelsFeed.items(); // getting reels, see "account-followers.feed.example.ts" if you want to know how to work with feeds
+	if (storyItems.length === 0) {
+		// we can check items length and find out if the user does have any story to watch
+		console.log(`${targetUser.username}'s story is empty`);
+		return;
+	}
+	const seenResult = await client.story.seen(storyItems);*/
+
+	logger.info("view stories %o", res);
+}
+
+export default story;
---
+++ src/streams/viewStory.ts
@@ -0,0 +1,73 @@
+import { Subject } from 'rxjs';
+import { TimelineFeedResponseMedia_or_ad } from 'instagram-private-api/dist/responses';
+import { chance, convertIDtoPost } from '../core/utils';
+import { store, addServerCalls } from '../core/store';
+import {
+	withLatestFrom,
+	filter,
+	flatMap,
+	share,
+	tap,
+	map,
+} from 'rxjs/operators';
+import { blacklistFilter, interestRate, media$ } from './utils';
+import logger from '../core/logging';
+
+export const viewStory$ = media$.pipe(
+	filter(media => {
+		logger.warn('Media info: %o', media)
+		// TODO just a test, could be removed if this seems to be true
+		// detecting ads and filter them out
+		if (media.ad_id || media.link) {
+			logger.warn('media was an ad with id: %s / link: %s', media.ad_id, media.link);
+			return false;
+		}
+
+		return true;
+	}),
+	share(),
+);
+
+export const viewedStory$ = viewStory$.pipe(
+	withLatestFrom(store.pluck('client')),
+	/*flatMap(async ([media, client]) => {
+		const { user } = config;
+		const response = await client.media.like({
+			mediaId: media.id,
+			moduleInfo: {
+				module_name: 'profile',
+				user_id: user.pk,
+				username: user.username,
+			},
+			// d means like by double tap (1), you cant unlike posts with double tap
+			d: chance(.5) ? 0 : 1,
+		});
+
+		return { media, response, config };
+	}),
+	tap(({ response }) => {
+		if (response.status != 'ok') {
+			logger.error('like response is not okay: %o', response);
+			logger.error('exiting process');
+			process.exit(1);
+		}
+	}),
+
+	// get current likes from store
+	withLatestFrom(store.pluck('imageLikes')),
+	map(([{ media, response, config }, imageLikes]) => ({ media, response, config, imageLikes })),
+
+	tap(({ media, response, config, imageLikes }) => {
+		let limit = config.likeLimit;
+		if (config.tags.length) limit *= config.tags.length;
+
+		logger.info('liked %d / %d - media: %s - response: %o', imageLikes + 1, limit, convertIDtoPost(media.id), response);
+		// increment image likes
+		store.setState({ imageLikes: imageLikes + 1 });
+	}),
+
+	// increment server calls for the like call
+	tap(() => addServerCalls(1)),*/
+
+	share(),
+);
