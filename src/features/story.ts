a6a9016ad7e582bd5bbc2a493297490ae8f577d3
Luca Reccia <lucareccia@hotmail.it>
2019-11-02 17:53:24 +0100

    fix stories

--- src/features/story.ts
+++ src/features/story.ts
@@ -1,16 +1,60 @@
-import { IgApiClient } from "instagram-private-api";
-import { Config } from "../core/config";
-import { store } from "../core/store";
-import logger from "../core/logging";
-import { Feed } from "instagram-private-api/dist/core/feed";
-import { StoryServiceInput } from "instagram-private-api/dist/types";
+import { IgApiClient } from 'instagram-private-api';
+import { Config } from '../core/config';
+import logger from '../core/logging';
 
-async function story(client: IgApiClient, config: Config): Promise<void> {
-	logger.info("starting with story feed");
+/**
+	This function will attempt to see all the stories of a given user set.
+	Input:
+	- client: client to use
+	- userIds: array of user's pk to see
+	- lastSeenPerUser: 
+		(optional) obj of the form {"user1.pk":"user1.seen","user2.pk":"user2.seen"}
+		from all the retrieved stories of an user only those taken after the specified `seen` attribute will be actually viewed.
+		For example this is used by storyMassView to avoid viewing multiple time stories.
+*/
+async function storyView(client: IgApiClient, userIds: number[], lastSeenPerUser?: any): Promise<void> {
+	try{
+		//get all the stories from the users picked above
+		const storyMediaFeed = client.feed.reelsMedia({
+			userIds,
+		});
+		let stories = await storyMediaFeed.items();
+		if (!stories.length) {
+			logger.info('selected users has no story!');
+			return;
+		}
+
+		if(lastSeenPerUser){
+			//from all the stories of the user only get those how i've not yet seen.
+			stories = stories.filter(
+				({ taken_at, user: { pk } }) => !lastSeenPerUser[pk] || taken_at > lastSeenPerUser[pk]
+			);
+		}
+
+		if (!stories.length) {
+			logger.info('no new story to view!');
+			return;
+		}
+
+		//view stories
+		await client.story.seen(stories);
+		logger.info('%i latest stories has been viewed', stories.length);
+	} catch(error){
+		logger.warn('error while viewing users stories: %o', error);
+		return;
+	}
+
+}
+
+/**
+	This function will attemp to view all the new (not yet seen) stories showed in the top of the timeline
+*/
+async function storyMassView(client: IgApiClient, config: Config): Promise<void> {
+	logger.info('starting vieweing stories from story feed');
 
 	try {
 		// get users who have stories to see in instagram tray (top bar)
-		const storyTrayFeed = client.feed.reelsTray("cold_start");
+		const storyTrayFeed = client.feed.reelsTray('cold_start');
 		let storyTrayItems = await storyTrayFeed.items();
 		storyTrayItems = storyTrayItems.filter(
 			(item: any) =>
@@ -18,37 +62,19 @@ async function story(client: IgApiClient, config: Config): Promise<void> {
 				item.user.username !== config.username
 		);
 		if (!storyTrayItems.length) {
-			logger.info("no story left to view!");
+			logger.info('no story left to view!');
 			return;
 		}
 
-		//get all the stories from the users picked above
-		const storyMediaFeed = client.feed.reelsMedia({
-			userIds: storyTrayItems.map(item => item.user.pk)
-		});
-		const stories = await storyMediaFeed.items();
-		if (!stories.length) {
-			logger.info("no story left to view!");
-			return;
-		}
-
-		//from all the stories of the user only get those how i've not yet seen.
-		let lastSeenPerUser: any = {};
+		const userIds: number[] = [], lastSeenPerUser: any = {};
 		storyTrayItems.map(
-			({ seen, user: { pk } }) => (lastSeenPerUser[pk] = seen)
-		);
-		stories.filter(
-			({ taken_at, user: { pk } }) => taken_at > lastSeenPerUser[pk]
+			({ seen, user: { pk } }) => {lastSeenPerUser[pk] = seen; userIds.push(pk)}
 		);
 
 		//view stories
-		await client.story.seen(stories);
-
-		logger.info("%i latest stories has been viewed", stories.length);
-		return;
+		await storyView(client, userIds, lastSeenPerUser)
 	} catch (error) {
-		logger.warn("error while viewing timeline stories: %o", error);
-		return;
+		logger.warn('error while viewing timeline stories: %o', error);
 	}
 }
 
