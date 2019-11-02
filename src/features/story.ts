c4e5699190f75e6cc85a38d6c47166d3af918508
Luca Reccia <lucareccia@hotmail.it>
2019-11-02 13:32:19 +0100

    asd

--- src/features/story.ts
+++ src/features/story.ts
@@ -8,39 +8,35 @@ import { StoryServiceInput } from 'instagram-private-api/dist/types';
 async function story(client: IgApiClient, config: Config): Promise<void> {
 	logger.info('starting with story feed');
 
+	// get users who have stories to see in instagram tray (top bar)
 	const storyTrayFeed = client.feed.reelsTray('cold_start');
-	let storyTray = await storyTrayFeed.items();
+	let storyTrayItems = await storyTrayFeed.items();
 
-	
-	logger.info('%o',storyTray.find(item=>item.user.username==='gaetanogenovesimaestro'));
-	storyTray = storyTray.filter(
-		(item: any) => item.seen === 0 && item.user.username !== config.username
+	const pizza = storyTrayItems.find(item => item.user.username === 'pizzanapoletanaofficial');
+	logger.warn('hello %s', pizza.seen);
+	logger.warn('hello %s', pizza.latest_reel_media);
+	logger.warn('hello %s', pizza.seen < pizza.latest_reel_media);
+
+	storyTrayItems = storyTrayItems.filter(
+		(item: any) => item.seen < item.latest_reel_media && item.user.username !== config.username
 	);
-
-	logger.info('%o',storyTray.find(item=>item.user.username==='gaetanogenovesimaestro'));
-
-	const storyMediaFeed = client.feed.reelsMedia({
-		userIds: storyTray.map(item => item.user.pk)
-	});
-
-	const stories = await storyMediaFeed.items();
-
-	const res = await client.story.seen(stories);
-
-	/*const targetUser = await client.user.searchExact("maniinpasta"); // getting exact user by login
-	const reelsFeed = client.feed.reelsMedia({
-		// working with reels media feed (stories feed)
-		userIds: [targetUser.pk] // you can specify multiple user id's, "pk" param is user id
-	});
-	const storyItems = await reelsFeed.items(); // getting reels, see "account-followers.feed.example.ts" if you want to know how to work with feeds
-	if (storyItems.length === 0) {
-		// we can check items length and find out if the user does have any story to watch
-		console.log(`${targetUser.username}'s story is empty`);
+	if(!storyTrayItems.length){
+		logger.info('No story left to view!');
 		return;
 	}
-	const seenResult = await client.story.seen(storyItems);*/
 
-	logger.info('view stories %o', res);
+	logger.warn('stories to see len: %o %o %o', storyTrayItems[0].user.username, storyTrayItems[1].user.username, storyTrayItems.length);
+	return;
+
+	//get stories from stories and view them
+	const storyMediaFeed = client.feed.reelsMedia({
+		userIds: storyTrayItems.map(item => item.user.pk)
+	});
+	const stories = await storyMediaFeed.items();
+	const res = await client.story.seen(stories);
+
+	return;
+
 }
 
 export default story;
