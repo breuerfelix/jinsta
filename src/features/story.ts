dcee31622befaa7893ecbf639b854eda2219ff4a
Luca Reccia <lucareccia@hotmail.it>
2019-11-02 17:28:13 +0100

    first implementation of stories

--- src/features/story.ts
+++ src/features/story.ts
@@ -1,42 +1,55 @@
-import { IgApiClient } from 'instagram-private-api';
-import { Config } from '../core/config';
-import { store } from '../core/store';
-import logger from '../core/logging';
-import { Feed } from 'instagram-private-api/dist/core/feed';
-import { StoryServiceInput } from 'instagram-private-api/dist/types';
+import { IgApiClient } from "instagram-private-api";
+import { Config } from "../core/config";
+import { store } from "../core/store";
+import logger from "../core/logging";
+import { Feed } from "instagram-private-api/dist/core/feed";
+import { StoryServiceInput } from "instagram-private-api/dist/types";
 
 async function story(client: IgApiClient, config: Config): Promise<void> {
-	logger.info('starting with story feed');
+	logger.info("starting with story feed");
 
-	// get users who have stories to see in instagram tray (top bar)
-	const storyTrayFeed = client.feed.reelsTray('cold_start');
-	let storyTrayItems = await storyTrayFeed.items();
+	try {
+		// get users who have stories to see in instagram tray (top bar)
+		const storyTrayFeed = client.feed.reelsTray("cold_start");
+		let storyTrayItems = await storyTrayFeed.items();
+		storyTrayItems = storyTrayItems.filter(
+			(item: any) =>
+				item.seen < item.latest_reel_media &&
+				item.user.username !== config.username
+		);
+		if (!storyTrayItems.length) {
+			logger.info("no story left to view!");
+			return;
+		}
 
-	const pizza = storyTrayItems.find(item => item.user.username === 'pizzanapoletanaofficial');
-	logger.warn('hello %s', pizza.seen);
-	logger.warn('hello %s', pizza.latest_reel_media);
-	logger.warn('hello %s', pizza.seen < pizza.latest_reel_media);
+		//get all the stories from the users picked above
+		const storyMediaFeed = client.feed.reelsMedia({
+			userIds: storyTrayItems.map(item => item.user.pk)
+		});
+		const stories = await storyMediaFeed.items();
+		if (!stories.length) {
+			logger.info("no story left to view!");
+			return;
+		}
 
-	storyTrayItems = storyTrayItems.filter(
-		(item: any) => item.seen < item.latest_reel_media && item.user.username !== config.username
-	);
-	if(!storyTrayItems.length){
-		logger.info('No story left to view!');
+		//from all the stories of the user only get those how i've not yet seen.
+		let lastSeenPerUser: any = {};
+		storyTrayItems.map(
+			({ seen, user: { pk } }) => (lastSeenPerUser[pk] = seen)
+		);
+		stories.filter(
+			({ taken_at, user: { pk } }) => taken_at > lastSeenPerUser[pk]
+		);
+
+		//view stories
+		await client.story.seen(stories);
+
+		logger.info("%i latest stories has been viewed", stories.length);
+		return;
+	} catch (error) {
+		logger.warn("error while viewing timeline stories: %o", error);
 		return;
 	}
-
-	logger.warn('stories to see len: %o %o %o', storyTrayItems[0].user.username, storyTrayItems[1].user.username, storyTrayItems.length);
-	return;
-
-	//get stories from stories and view them
-	const storyMediaFeed = client.feed.reelsMedia({
-		userIds: storyTrayItems.map(item => item.user.pk)
-	});
-	const stories = await storyMediaFeed.items();
-	const res = await client.story.seen(stories);
-
-	return;
-
 }
 
 export default story;
